f881be18f16a0ca8ca94b82a4eaa257b0ab357e4
tanvirAR <110296757+tanvirAR@users.noreply.github.com>
2023-05-03 11:35:43 +0600

    Update EmailList.tsx

--- Components/homePage/EmailList.tsx
+++ Components/homePage/EmailList.tsx
@@ -7,37 +7,7 @@ import TempSingleEmail from "../common/singlemail/SingleMail";
 
 export default function EmailList(props: any) {
   const {emailList} = props;
-//   const {data, error, isError, isLoading, isSuccess} = useGetAllMailsQuery();
-
-//   let emailList;
-//   // console.log(data?.mails);
-//   // console.log(error);
-
-
   
-//  if(isError){
-//   emailList = <p>Error occured! Please try again.</p>
-//  }
-
-//   if (data) {
-//     emailList = data.mails.map((email: email) => (
-//       <SingleEmail property={email} key={email.id} />
-//     ));
-
-//     // emailList = data.mails.map((email: email) =>{ return <SingleEmail key={email.id} property={email}  /> }   )
-//   }
-
-//  if (isLoading){
-//   emailList = <p>Loading...</p>
-//  }
-
-//   if(data &&  data.mails.length == 0){
-//     emailList = <p>No Email found!</p>
-//   }
-
-
-  // console.log(emailList)
-
   return (
     <div  >
       {emailList}
