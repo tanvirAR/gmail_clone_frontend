7f48336116cb13e270a66f0e3efe99a696fe33f7
Tanvir AR <tanvir.ar.47@gmail.com>
2023-06-05 09:27:23 +0600

    Update EmailList.tsx

--- Components/homePage/EmailList.tsx
+++ Components/homePage/EmailList.tsx
@@ -1,21 +1,7 @@
-import { useGetAllMailsQuery } from "../../features/email/emailApi";
-import { email } from "../../interface/singleMail.interface";
-import SingleEmail from "./SingleEmail";
-import TempSingleEmail from "../common/singlemail/SingleMail";
-
-
+import styles from "./InboxEmails.module.css"
 
 export default function EmailList(props: any) {
-  const {emailList} = props;
-  
-  return (
-    <div  >
-      {emailList}
-      <TempSingleEmail />
-      <TempSingleEmail />
-      <TempSingleEmail />
-      <TempSingleEmail />
-      <TempSingleEmail />
-    </div>
-  );
+  const { emailList } = props;
+
+  return <div className={styles.cont}>{emailList}</div>;
 }
