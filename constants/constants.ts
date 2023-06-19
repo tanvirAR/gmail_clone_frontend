d206163705e662f7c9a44e975cd73e3e494ed287
Tanvir AR <tanvir.ar.47@gmail.com>
2023-06-19 08:29:22 +0600

    Update constants.ts

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,4 +1,4 @@
 export const accountNumber: number = 1;
 
 /* used for cache validation timeout in RTK query */
-export const keepUnusedDataInSeconds: number = 60*60;  // 1 hour default
+export const keepUnusedDataInSeconds: number = 60*60;  // 1 hour is default
